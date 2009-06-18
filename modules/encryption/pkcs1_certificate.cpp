ce75be2ce297e8048a5e1c7d79b4f7bcc7571dbc
Bartłomiej Zimoń <uzi18@go2.pl>
2009-06-18 21:27:20 +0000

    encryption - Copy/Paste error ;) (tomek-k)
    git-svn-id: svn+ssh://kadu.net/home/kadu/repos/kadu/branches/0.6.5@6742 fea5205d-6df5-0310-bef7-ea67e47da9a5

--- modules/encryption/pkcs1_certificate.cpp
+++ modules/encryption/pkcs1_certificate.cpp
@@ -384,7 +384,7 @@ bool PKCS1Certificate::extractPrivateKey(const QCA::SecureArray &certificate, QC
 	//read the public exponent
 	QCA::SecureArray eData(length);
 	for(uint64_t i = 0; i < length; i++)
-		eData[i] = ((uint64_t)readNextOctet()) << ((i-1)*8);
+		eData[i] = readNextOctet();
 	e.fromArray(eData);
 
 	octet = readNextOctet();
@@ -547,9 +547,9 @@ uint64_t PKCS1Certificate::readDefiniteLength()
 		}
 
 		//construct the length value
-		quint64 length = 0;
-                for (quint8 i = lengthOctetsCount; i > 0; i--)
-                        length |= ((quint64)readNextOctet()) << ((i-1)*8);
+		uint64_t length = 0;
+		for(uint8_t i = lengthOctetsCount; i > 0; i--)
+			length |= ((uint64_t)readNextOctet()) << ((i-1)*8);
 
 		//the SecureArray stores it's size in an int variable, so we must check
 		//if the read length is not greater than the maximum value that int can
